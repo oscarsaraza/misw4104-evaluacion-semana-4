0858f2869452fa716d9ad688424cf6700fce3a6a
Oscar Saraza <oscarsaraza@gmail.com>
2025-04-25 14:06:28 -0500

    Actualización de encabezado de la aplicación.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'misw4104-evaluacion-semana-4' title`, () => {
+  it(`should have the 'TuSegundazo.com' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('misw4104-evaluacion-semana-4');
+    expect(app.title).toEqual('TuSegundazo.com');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, misw4104-evaluacion-semana-4');
+    expect(compiled.querySelector('h1')?.textContent).toContain('TuSegundazo.com');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'misw4104-evaluacion-semana-4';
+  title = 'TuSegundazo.com';
 }
