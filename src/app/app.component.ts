f8f2b1e4584110f7f000979befb00f84720bac29
Oscar Saraza <oscarsaraza@gmail.com>
2025-04-26 18:56:49 -0500

    Agregar texto de pie de página.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaVehiculosComponent } from "./lista-vehiculos/lista-vehiculos.component";
+import { ListaVehiculosComponent } from './lista-vehiculos/lista-vehiculos.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ListaVehiculosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TuSegundazo.com';
+  textoFooter =
+    'Contact us: +57 3102105253 - info@tusegundazo.com - @tusegundazo';
 }
