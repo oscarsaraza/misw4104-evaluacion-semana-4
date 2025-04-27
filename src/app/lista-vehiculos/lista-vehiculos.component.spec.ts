dcd6672dac19814e007908e1285f83927d1d8bc6
Oscar Saraza <oscarsaraza@gmail.com>
2025-04-26 21:39:18 -0500

    Implementación de prueba de componente ListaVehiculos

--- src/app/lista-vehiculos/lista-vehiculos.component.spec.ts
+++ src/app/lista-vehiculos/lista-vehiculos.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { ListaVehiculosComponent } from './lista-vehiculos.component';
 
 describe('ListaVehiculosComponent', () => {
   let component: ListaVehiculosComponent;
   let fixture: ComponentFixture<ListaVehiculosComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListaVehiculosComponent]
-    })
-    .compileComponents();
+      imports: [ListaVehiculosComponent, HttpClientModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListaVehiculosComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      {
+        id: 1,
+        marca: 'Renault',
+        linea: 'Kangoo',
+        referencia: 'Kangoo 1.6',
+        modelo: 2020,
+        kilometraje: 20000,
+        color: 'Blanco',
+        imagen: '',
+      },
+      {
+        id: 1,
+        marca: 'Toyota',
+        linea: 'Corolla',
+        referencia: 'Corolla 1.8',
+        modelo: 2020,
+        kilometraje: 20000,
+        color: 'Rojo',
+        imagen: '',
+      },
+      {
+        id: 1,
+        marca: 'Chevrolet',
+        linea: 'Onix',
+        referencia: 'Onix 1.0',
+        modelo: 2020,
+        kilometraje: 20000,
+        color: 'Azul',
+        imagen: '',
+      },
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('Crea el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('La tabla se crea con tres filas y el encabezado', () => {
+    // 1 fila de encabezado + 3 filas de vehiculos
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4);
   });
 });
--- src/app/vehiculos.service.spec.ts
+++ src/app/vehiculos.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { VehiculosService } from './vehiculos.service';
 
 describe('VehiculosService', () => {
   let service: VehiculosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(VehiculosService);
   });
 
